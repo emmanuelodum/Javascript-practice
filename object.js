a1ffdc9845fc0644961ddc9760b9496da8f5b751
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-12-03 23:27:54 +0100

    Object e.g

--- object.js
+++ object.js
@@ -14,3 +14,7 @@ car.printDescription();
 console.log(car.year + " price: 5000 ");
 console.log(car["year"]);
 console.log(car[1]);
+
+var anotherCar = {};
+anotherCar.enzo = 'byke';
+console.log(anotherCar.enzo);
