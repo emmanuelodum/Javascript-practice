0aaeb45009ffeb8678f70af0eae6f952673365f9
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-12-29 20:04:50 +0100

    expresion walkthrough

--- functions-expresion.js
+++ functions-expresion.js
@@ -114,8 +114,14 @@ function whisper(str) {
 console.log(whisper("Hey Buddy")); // => "...hey buddy..."
 console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
 console.log(whisper("Hey Buddy")); // => "...hey buddy..."
-console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
-console.log(whisper("Hey Buddy")); // => "...hey buddy..."
-console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
-console.log(whisper("Hey Buddy")); // => "...hey buddy..."
-console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
+
+
+function averageOfFour(num1, num2, num3, num4) {
+  let sum = num1 + num2 + num3 + num4;
+  let average = sum / 4;
+  return average;
+}
+
+console.log(averageOfFour(10, 10, 15, 5)); // => 10
+console.log(averageOfFour(3, 10, 11, 4)); // => 7
+console.log(averageOfFour(1, 2, 3, 4)); // => 2.5
