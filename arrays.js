2ed199d7c6d5ab92a54b5f9df43f25b303fe07d7
Emmanuel Odum <emmanuelodum48@gmail.com>
2023-01-18 13:18:59 +0100

    arrays additional

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 let a = [1, 2, 'true', 4, 5, 'false'];
 let b = ['emma', 'dan', 'james', 'ben'];
-a[0] = 'hello world';
+a[0] = 'hello world', 'welcom';
 console.log(a.length) // gives actual number in an Arrays NOT zero based numbers.
 console.log(a[0]);
 
